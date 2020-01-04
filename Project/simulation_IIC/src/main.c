da843746d0c798d279e7d07805e2b47f1dbf2edb
bai-bit <1637274023@qq.com>
2020-01-04 21:30:04 +0800

    first commit

--- Project/simulation_IIC/src/main.c
+++ Project/simulation_IIC/src/main.c
@@ -7,16 +7,16 @@
 
 int main(int argc,const char *argv[])
 {
-	sysclk_PLLEN(PLLCLK_MUL_192MHz);
+    sysclk_PLLEN(PLLCLK_MUL_192MHz);
     uint8_t buf[SIZE] = "ladies and gentlemen,good everyone";
     uint8_t rbuf[SIZE] = {};
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	DelayInit();
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    DelayInit();
     GPIO_Init(HW_GPIOA, GPIO_PIN_9,GPIO_Mode_AF_PP);
-	GPIO_Init(HW_GPIOA, GPIO_PIN_10,GPIO_Mode_IN_FLOATING);
-	UART_Init(HW_USART1,BAUD_115200);
-	
-	AT24CXX_Init();
+    GPIO_Init(HW_GPIOA, GPIO_PIN_10,GPIO_Mode_IN_FLOATING);
+    UART_Init(HW_USART1,BAUD_115200);
+    
+    AT24CXX_Init();
     
     if(AT24CXX_check())
     {
