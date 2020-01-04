1801768d6882263cab2c638c4aecfa6be75c0f7d
bai-bit <1637274023@qq.com>
2020-01-04 21:34:52 +0800

    first commit

--- Libraries/drivers/src/gpio_iic.c
+++ Libraries/drivers/src/gpio_iic.c
@@ -3,135 +3,135 @@
 
 void iic_init(void)
 {
-	//initialization iic
-	//initialization gpio
-	//SDA PB9   SCL  PB8
+    //initialization iic
+    //initialization gpio
+    //SDA PB9   SCL  PB8
+    
+    GPIO_Init(HW_GPIOB,GPIO_PIN_8,GPIO_Mode_Out_PP );
+    GPIO_Init(HW_GPIOB,GPIO_PIN_9,GPIO_Mode_Out_PP );
 	
-	GPIO_Init(HW_GPIOB,GPIO_PIN_8,GPIO_Mode_Out_PP );
-	GPIO_Init(HW_GPIOB,GPIO_PIN_9,GPIO_Mode_Out_PP );
-	
-	GPIO_PinWrite(HW_GPIOB,GPIO_PIN_8,1);
-	GPIO_PinWrite(HW_GPIOB,GPIO_PIN_9,1);
+    GPIO_PinWrite(HW_GPIOB,GPIO_PIN_8,1);
+    GPIO_PinWrite(HW_GPIOB,GPIO_PIN_9,1);
 }
 
 void iic_start(void)
 {
-	IIC_SDA_OUT;
-	
-	IIC_SDA = 1;
-	IIC_SCL = 1;
-	delayus(5);
-	IIC_SDA = 0;
-	delayus(5);
-	IIC_SCL = 0;
+    IIC_SDA_OUT;
+    
+    IIC_SDA = 1;
+    IIC_SCL = 1;
+    delayus(5);
+    IIC_SDA = 0;
+    delayus(5);
+    IIC_SCL = 0;
 }
 
 void iic_stop(void)
 {
-	IIC_SDA_OUT;
-
-	IIC_SCL = 0;
-	IIC_SDA = 0;
-	delayus(5);
-	IIC_SCL = 1;
-	
-	IIC_SDA = 1;
-	delayus(5);
+    IIC_SDA_OUT;
+    
+    IIC_SCL = 0;
+    IIC_SDA = 0;
+    delayus(5);
+    IIC_SCL = 1;
+    
+    IIC_SDA = 1;
+    delayus(5);
 }
 
 uint8_t iic_wait_ack(void)
 {
-	uint32_t timeout = 0x01000;
-	uint8_t ret = 0;
+    uint32_t timeout = 0x01000;
+    uint8_t ret = 0;
     IIC_SDA_IN;
-
-	IIC_SDA = 1;
-	delayus(2);
-	IIC_SCL = 1;
-	delayus(2);
-  	while(READ_SDA)
-	{
-		timeout--;
-		if(timeout == 0)
-		{
-			iic_stop();
-			return 0;
-		}
+    
+    IIC_SDA = 1;
+    delayus(2);
+    IIC_SCL = 1;
+    delayus(2);
+    while(READ_SDA)
+    {
+        timeout--;
+        if(timeout == 0)
+        {
+            iic_stop();
+            return 0;
+        }
         printf("B");
-	}
-	IIC_SCL = 0;
-	ret = 1;
-	return ret;
+    }
+    IIC_SCL = 0;
+    ret = 1;
+    return ret;
 }
 
 void iic_ack(void)
 {
     IIC_SCL = 0;
-	IIC_SDA_OUT;
-	
-	delayus(2);
-	IIC_SDA = 0;
-	delayus(2);
-	IIC_SCL = 1;
-	delayus(5);
-	IIC_SDA_IN;
+    IIC_SDA_OUT;
+    
+    delayus(2);
+    IIC_SDA = 0;
+    delayus(2);
+    IIC_SCL = 1;
+    delayus(5);
+    IIC_SDA_IN;
 }
 
 void iic_notack(void)
 {
     IIC_SCL = 0;
-	IIC_SDA_OUT;
-	
-	delayus(2);
-	IIC_SDA = 1;
-	delayus(2);
-	IIC_SCL = 1;
-	delayus(5);
-	IIC_SCL = 0;
+    IIC_SDA_OUT;
+    
+    delayus(2);
+    IIC_SDA = 1;
+    delayus(2);
+    IIC_SCL = 1;
+    delayus(5);
+    IIC_SCL = 0;
 }
 
 void iic_send_data(uint8_t data)
 {
-	uint8_t i = 0;
-	
-	IIC_SDA_OUT;
-	IIC_SCL = 0;
-	delayus(2);
-	for(i = 0;i < 8;i++)
-	{
-		if((data & 0x80) >> 7)
-			IIC_SDA = 1;
-		else
-			IIC_SDA = 0;
+    uint8_t i = 0;
+    
+    IIC_SDA_OUT;
+    IIC_SCL = 0;
+    delayus(2);
+    for(i = 0;i < 8;i++)
+    {
+        if((data & 0x80) >> 7)
+            IIC_SDA = 1;
+        else
+            IIC_SDA = 0;
         data <<= 1;
-		delayus(2);
-		IIC_SCL = 1;
-		delayus(2);
-		IIC_SCL = 0;
-		delayus(2);
-	}
+        delayus(2);
+        IIC_SCL = 1;
+        delayus(2);
+        IIC_SCL = 0;
+        delayus(2);
+    }
 }
 
 uint8_t iic_receive_data(uint8_t ack)
 {
-	uint8_t ret = 0;
-	uint8_t i = 0;
-	IIC_SDA_IN;
-	for(i = 0;i < 8;i++)
-	{
-		IIC_SCL = 0;
-		delayus(2);
-		IIC_SCL = 1;
-		delayus(2);
-        ret <<= 1;
-		if(READ_SDA)
-			ret++;
+    uint8_t ret = 0;
+    uint8_t i = 0;
+    IIC_SDA_IN;
+    for(i = 0;i < 8;i++)
+    {
+        IIC_SCL = 0;
         delayus(2);
-	}
-	if(ack)
-		iic_ack();
-	else
-		iic_notack();
-	
-	return ret;
+        IIC_SCL = 1;
+        delayus(2);
+        ret <<= 1;
+        if(READ_SDA)
+            ret++;
+        delayus(2);
+    }
+    if(ack)
+        iic_ack();
+    else
+        iic_notack();
+    
+    return ret;
 }
